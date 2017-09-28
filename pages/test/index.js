d5754ec170633993154972287ef414b7b45b848e
yyma <1007314628@qq.com>
2017-09-28 23:46:15 +0800

    tijiao

---
+++ pages/test/index.js
@@ -0,0 +1,94 @@
+//index.js
+//获取应用实例
+var app = getApp();
+Page({
+  data: {
+    currentTab: 0, 
+    hasMoreSelect: false,
+    selectSize: "选择：",
+    selectSizePrice: 0,
+    shopNum: 0,
+    hideShopPopup: true,
+    buyNumber: 0,
+    buyNumMin: 1,
+    buyNumMax: 0
+  },
+  onLoad: function (e) {
+    console.log('onLoad');
+    var that = this;
+  },
+  goShopCar: function () {
+ 
+  },
+  toAddShopCar: function () {
+  ;
+  },
+  tobuy: function () {
+    this.setData({
+      shopType: "tobuy"
+    });
+    this.bindGuiGeTap();
+   
+  },
+  /**
+   * 规格选择弹出框
+   */
+  bindGuiGeTap: function () {
+    this.setData({
+      hideShopPopup: false
+    })
+  },
+  /**
+   * 规格选择弹出框隐藏
+   */
+  closePopupTap: function () {
+    this.setData({
+      hideShopPopup: true
+    })
+  },
+  numJianTap: function () {
+    if (this.data.buyNumber > this.data.buyNumMin) {
+      var currentNum = this.data.buyNumber;
+      currentNum--;
+      this.setData({
+        buyNumber: currentNum
+      })
+    }
+  },
+  numJiaTap: function () {
+    if (this.data.buyNumber < this.data.buyNumMax) {
+      var currentNum = this.data.buyNumber;
+      currentNum++;
+      this.setData({
+        buyNumber: currentNum
+      })
+    }
+  },
+  /**
+  * 加入购物车
+  */
+  addShopCar: function () {
+    if (this.data.buyNumber < 1) {
+      wx.showModal({
+        title: '提示',
+        content: '购买数量不能为0！',
+        showCancel: false
+      })
+      return;
+    }
+  },
+	/**
+	  * 立即购买
+	  */
+  buyNow: function () {
+    if (this.data.buyNumber < 1) {
+      wx.showModal({
+        title: '提示',
+        content: '购买数量不能为0！',
+        showCancel: false
+      })
+      return;
+    }
+
+  }
+})
